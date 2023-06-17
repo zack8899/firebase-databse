23b5e4ddee35faf8d1af0aa54339f2d0a8008843
abdirizak abdullahi <122808527+zack8899@users.noreply.github.com>
2023-06-17 03:22:50 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyD87dn4Nb-FQ8eo-Mj0-4mOSYjCQns2zeA",
-    authDomain: "jackan-f7100.firebaseapp.com",
-    databaseURL: "https://jackan-f7100-default-rtdb.firebaseio.com",
-    projectId: "jackan-f7100",
-    storageBucket: "jackan-f7100.appspot.com",
-    messagingSenderId: "677584601589",
-    appId: "1:677584601589:web:76152cef2d7cf43e93e0bc"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-const database = getDatabase(app);
 
 
-database.ref('users/'+'people').set({
-    name:'zack ma ali',
 
-})
+window.addEventListener('load', () => {
+  if (localStorage.getItem('reg') == 'true') {
+    document.querySelector('.reg-data-form').classList.add('hide');
+  }
+});
+
+document.querySelector('.reg-data-form form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  let name = document.querySelector('.reg-data-form input').value;
+  localStorage.setItem('Name', name);
+
+  document.querySelector('.reg-data-form').classList.add('hide');
+  localStorage.setItem('reg', true);
+});
+
+const addPost = document.querySelector('.add-post'),
+addForm = document.querySelector('.post-data-form'),
+closeBtn = document.querySelector('.post-data-form .close')
+
+addPost.onclick = ()=>{
+  addForm.classList.toggle('hide');
+}
+closeBtn.onclick = ()=>{
+  addForm.classList.toggle('hide');
+}
+
+
